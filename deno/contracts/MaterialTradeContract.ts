ebd07235901d4699aee6079f9af04b4af7cec965
yjgaia <yjdev88@gmail.com>
2024-10-04 14:58:27 +0900

    Refactor MaterialTradeContract to import EthersContract from ethers@6.7.0

--- deno/contracts/MaterialTradeContract.ts
+++ deno/contracts/MaterialTradeContract.ts
@@ -1,4 +1,7 @@
-import { Signer } from "https://esm.sh/ethers@6.7.0";
+import {
+  Contract as EthersContract,
+  Signer,
+} from "https://esm.sh/ethers@6.7.0";
 import { Contract } from "https://raw.githubusercontent.com/yjgaia/contract-module/refs/heads/main/deno/contract.ts";
 import MaterialTradeArtifact from "./abi/MaterialTrade.json" assert {
   type: "json",
@@ -9,11 +12,11 @@ export default class MaterialTradeContract extends Contract<MaterialTrade> {
   constructor(signer: Signer, address: string) {
     super(signer, address);
 
-    this.ethersContract = new Contract(
+    this.ethersContract = new EthersContract(
       this.address,
       MaterialTradeArtifact.abi,
       this.signer,
-    ) as any;
+    );
 
     this.eventFilters = {
       MaterialCreated: this.ethersContract.filters.MaterialCreated(),
