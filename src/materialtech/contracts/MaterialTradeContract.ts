e13eb267b764120426e9a4d8f36a4d9142721375
yjgaia <yjdev88@gmail.com>
2024-10-05 12:23:04 +0900

    Fix path

--- src/materialtech/contracts/MaterialTradeContract.ts
+++ src/materialtech/contracts/MaterialTradeContract.ts
@@ -1,5 +1,5 @@
 import { Contract } from "@common-module/contract";
-import { JsonRpcSigner } from "ethers";
+import { EventLog, JsonRpcSigner } from "ethers";
 import { MaterialTrade } from "./abi/MaterialTrade.js";
 import MaterialTradeArtifact from "./abi/MaterialTrade.json" assert {
   type: "json",
@@ -15,10 +15,19 @@ export default class MaterialTradeContract extends Contract<MaterialTrade> {
     signer: JsonRpcSigner,
     name: string,
     symbol: string,
-  ): Promise<void> {
-    await this.executeAndWait(
+  ): Promise<string> {
+    const { receipt } = await this.executeAndWait(
       signer,
       (contract) => contract.createMaterial(name, symbol),
     );
+
+    if (!receipt) throw new Error("Transaction failed");
+
+    for (const log of receipt.logs) {
+      if (log instanceof EventLog && log.fragment.name === "MaterialCreated") {
+        return log.args[1];
+      }
+    }
+    throw new Error("MaterialCreated event not found");
   }
 }
