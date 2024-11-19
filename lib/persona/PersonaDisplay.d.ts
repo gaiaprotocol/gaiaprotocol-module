c3935bb2c2ae9c5f6e97bafc9208fac8ee31d2e8
yjgaia <yjdev88@gmail.com>
2024-11-19 15:32:50 +0900

    Add walletAddress property and loadPersona method to PersonaDisplay for fetching persona data

--- lib/persona/PersonaDisplay.d.ts
+++ lib/persona/PersonaDisplay.d.ts
@@ -1,5 +1,7 @@
 import { DomNode } from "@common-module/app";
 export default class PersonaDisplay extends DomNode {
+    private walletAddress;
     constructor(walletAddress: string);
+    private loadPersona;
 }
 //# sourceMappingURL=PersonaDisplay.d.ts.map
--- src/persona/PersonaDisplay.ts
+++ src/persona/PersonaDisplay.ts
@@ -1,8 +1,15 @@
 import { DomNode } from "@common-module/app";
+import PersonaRepository from "./PersonaRepository.js";
 
 export default class PersonaDisplay extends DomNode {
-  constructor(walletAddress: string) {
+  constructor(private walletAddress: string) {
     super(".persona-display");
     this.append("Persona display for wallet address: ", walletAddress);
+    this.loadPersona();
+  }
+
+  private async loadPersona() {
+    const persona = await PersonaRepository.fetchPersona(this.walletAddress);
+    console.log(persona);
   }
 }
