f8cf88498a7f83e9cd8f4bf5b231a74a6a409c92
yjgaia <yjdev88@gmail.com>
2024-12-05 21:04:07 +0900

    Add GameEntity export to index files and update source maps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { default as LogoInput } from "./form/LogoInput.js";
 export { default as GaiaNameRepository } from "./gaia-names/GaiaNameRepository.js";
 export { default as GaiaProtocolConfig } from "./GaiaProtocolConfig.js";
+export { default as GameEntity } from "./game/GameEntity.js";
 export { default as GodMode } from "./GodMode.js";
 export { default as NFTList } from "./nft/NFTList.js";
 export { default as OpenSeaNFTData } from "./opensea/OpenSeaNFTData.js";
@@ -8,3 +9,4 @@ export { default as PersonaAvatar } from "./persona/PersonaAvatar.js";
 export { default as PersonaDisplay } from "./persona/PersonaDisplay.js";
 export { default as PersonaEntity } from "./persona/PersonaEntity.js";
 export { default as PersonaRepository } from "./persona/PersonaRepository.js";
+
