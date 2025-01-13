daba8bd04c741c1fac348918a6ad1cec2a913153
yjgaia <yjdev88@gmail.com>
2025-01-13 13:51:06 +0900

    Add PersonaPostRepository export to index files

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,12 @@
 export { default as ClanRepository } from "./clan/ClanRepository.js";
 export { default as ClanEmblemsContract } from "./clan/contracts/ClanEmblemsContract.js";
 export { default as ContractEventsProcessor } from "./core/ContractEventsProcessor.js";
+export { default as GaiaProtocolConfig } from "./core/GaiaProtocolConfig.js";
+export { default as GodMode } from "./core/GodMode.js";
 export { default as LogoInput } from "./form/LogoInput.js";
 export { default as GaiaNameRepository } from "./gaia-names/GaiaNameRepository.js";
-export { default as GaiaProtocolConfig } from "./core/GaiaProtocolConfig.js";
 export { default as GameDataManager } from "./game/GameDataManager.js";
 export { default as GameEntity } from "./game/GameEntity.js";
-export { default as GodMode } from "./core/GodMode.js";
 export { default as MaterialContract } from "./material/contracts/MaterialContract.js";
 export { default as MaterialFactoryContract } from "./material/contracts/MaterialFactoryContract.js";
 export { default as MaterialDataManager } from "./material/MaterialDataManager.js";
@@ -18,4 +18,5 @@ export { default as PersonaAvatar } from "./persona/PersonaAvatar.js";
 export { default as PersonaDisplay } from "./persona/PersonaDisplay.js";
 export { default as PersonaEntity } from "./persona/PersonaEntity.js";
 export { default as PersonaRepository } from "./persona/PersonaRepository.js";
+export { default as PersonaPostRepository } from "./persona/post/PersonaPostRepository.js";
 //# sourceMappingURL=index.d.ts.map
