6480ae3bba13c371b62b01d8f8d83e4a06d2f50a
yjgaia <yjdev88@gmail.com>
2025-01-13 11:36:52 +0900

    Add writePost method to PersonaPostRepository for posting content

--- src/persona/post/PersonaPostRepository.ts
+++ src/persona/post/PersonaPostRepository.ts
@@ -1,9 +1,17 @@
 import { SupabaseDataRepository } from "@common-module/supabase";
+import GaiaProtocolConfig from "../../core/GaiaProtocolConfig.js";
 import PersonaPostEntity, { PersonaPostQuery } from "./PersonaPostEntity.js";
 
 class PersonaPostRepository extends SupabaseDataRepository<PersonaPostEntity> {
   constructor() {
     super("persona_posts", PersonaPostQuery);
+  }
+
+  public async writePost(title: string, content: string): Promise<void> {
+    await GaiaProtocolConfig.supabaseConnector.callEdgeFunction(
+      "write-persona-post",
+      { title, content },
+    );
   }
 }
 
