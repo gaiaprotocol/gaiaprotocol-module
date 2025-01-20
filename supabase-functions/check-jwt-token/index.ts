3992edf8bf2f353f460121c928aaa884740cb941
yjgaia <yjdev88@gmail.com>
2025-01-20 18:20:32 +0900

    Refactor check-jwt-token function to use async/await for improved readability

--- supabase-functions/check-jwt-token/index.ts
+++ supabase-functions/check-jwt-token/index.ts
@@ -1,6 +1,4 @@
 import { serve } from "https://raw.githubusercontent.com/yjgaia/deno-module/refs/heads/main/api.ts";
 import { extractWalletAddressFromRequest } from "https://raw.githubusercontent.com/yjgaia/wallet-login-module/refs/heads/main/deno/auth.ts";
 
-serve((req) => {
-  return extractWalletAddressFromRequest(req);
-});
+serve(async (req) => await extractWalletAddressFromRequest(req));
