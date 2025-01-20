293ded805872fc1eb1b00390104f08238441ba37
yjgaia <yjdev88@gmail.com>
2025-01-20 13:44:58 +0900

    Update GaiaProtocolConfig to set mainnet URL and key; add TODO comments for future updates

--- src/core/GaiaProtocolConfig.ts
+++ src/core/GaiaProtocolConfig.ts
@@ -35,12 +35,15 @@ class GaiaProtocolConfig {
   public isTestnet = false;
 
   private supabaseUrls = {
-    mainnet: "",
+    //TODO: Update the mainnet URL
+    mainnet: "https://vykzkqqncxcfzflpkcsr.supabase.co",
     testnet: "https://vykzkqqncxcfzflpkcsr.supabase.co",
   };
 
   private supabaseKeys = {
-    mainnet: "",
+    //TODO: Update the mainnet key
+    mainnet:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5a3prcXFuY3hjZnpmbHBrY3NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk0MDc0OTUsImV4cCI6MjA0NDk4MzQ5NX0.UEGqZvIJ_FPxBk41C0RG4HfHahtR0yUfYVmtiZf61i0",
     testnet:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5a3prcXFuY3hjZnpmbHBrY3NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk0MDc0OTUsImV4cCI6MjA0NDk4MzQ5NX0.UEGqZvIJ_FPxBk41C0RG4HfHahtR0yUfYVmtiZf61i0",
   };
