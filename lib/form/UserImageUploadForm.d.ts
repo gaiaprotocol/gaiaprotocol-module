6e7e0b1e4d89a0f013866e13490bdcfa55fe56df
yjgaia <yjdev88@gmail.com>
2025-02-16 13:11:11 +0900

    Add UserImageUploadForm component for image upload functionality

---
+++ lib/form/UserImageUploadForm.d.ts
@@ -0,0 +1,6 @@
+import { DomNode } from "@common-module/app";
+export default class UserImageUploadForm extends DomNode {
+    constructor();
+    private uploadImage;
+}
+//# sourceMappingURL=UserImageUploadForm.d.ts.map
