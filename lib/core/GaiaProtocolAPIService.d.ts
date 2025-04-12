8a38c01af7b96aeb3508af4c2abec9a3bedb9e95
yjgaia <yjdev88@gmail.com>
2025-04-12 14:54:05 +0900

    Add GaiaProtocolAPIService class and update dependencies

---
+++ lib/core/GaiaProtocolAPIService.d.ts
@@ -0,0 +1,5 @@
+declare class GaiaProtocolAPIService {
+}
+declare const _default: GaiaProtocolAPIService;
+export default _default;
+//# sourceMappingURL=GaiaProtocolAPIService.d.ts.map
---
+++ src/core/GaiaProtocolAPIService.ts
@@ -0,0 +1,4 @@
+class GaiaProtocolAPIService {
+}
+
+export default new GaiaProtocolAPIService();
